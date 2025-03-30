120d425a6ffa71063ae762a10b306fec0d4ff0cb
João Víctor Guedes <joaoguedes1404@gmail.com>
2025-03-30 20:34:21 -0300

    feat:Adicionando botão e texto principal

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -12,6 +12,18 @@ export default function Home() {
           className="w-full h-full object-cover object-top absolute -z-10"
         />
         <NavBar currentPage="Home" />
+        <div className="flex flex-col justify-center items-center h-full w-full gap-y-10">
+          <h1 className="text-3xl mt-8 mb-10 font-instrument-serif">
+            Lorem ipsum dolor sit amet.
+          </h1>
+          <p className="text-lg mt-4 w-1/2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita,
+            mollitia!
+          </p>
+          <button className="py-3 px-8 bg-red-500 rounded-2xl text-white font-instrument-serif text-xl cursor-pointer hover:bg-red-700 transition duration-300 ease-in-out">
+            See our menu
+          </button>
+        </div>
       </div>
     </ContainerFull>
   );
