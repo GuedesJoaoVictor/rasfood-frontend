946b904e0d272b5b0cc71f9727dc55cb6dac8442
João Víctor Guedes <joaoguedes1404@gmail.com>
2025-04-01 15:06:28 -0300

    feat: Avançando na criação da tela home
    Adicionando seção de about us e de introdução

---
+++ src/pages/home/components/AboutUs/index.tsx
@@ -0,0 +1,27 @@
+import { Title } from "../../../../components/Title";
+import pizza from "../../../../assets/pizza.png";
+
+function AboutUs() {
+  return (
+    <div className="container mx-auto py-4 mt-10">
+      <div className="flex flex-row">
+        <div className="flex flex-col items-center justify-center text-center gap-y-10 w-1/2 h-full">
+          <Title font="Nunito">About Us</Title>
+          <p className="w-1/2 text-justify">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est eos,
+            harum sequi consequuntur quae ipsum, distinctio incidunt ipsam
+            repudiandae laudantium veniam. Autem non eum cumque consectetur
+            soluta quaerat reiciendis quo consequuntur aut? Veniam beatae
+            reprehenderit illum fugiat vitae cupiditate, optio ut dolor porro
+            quis iure.
+          </p>
+        </div>
+        <div className="w-1/2 h-full">
+          <img src={pizza} alt="pizza" className="w-96" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AboutUs;
